ec4a15280f3ca1866c243a87c96a4d68a4fc1dfc
Guilherme Ribeiro Maia <guilherme.maia@live.com>
2020-09-29 21:40:05 -0300

    Add programas
    Add programas de transformações de variaveis categoricas

---
+++ get_dummie.py
@@ -0,0 +1,41 @@
+def get_dummie(dataframe, name_missing = 'missing', cardinality = 15, drop_first = False):
+        
+    #Pegando somente as categoricas
+    dataframe_cat = dataframe[dataframe.select_dtypes('object').columns]
+        
+    if len(dataframe_cat.columns) == 0:
+        return dataframe_cat
+                
+    #Tratando missing se existir
+    dataframe_cat = dataframe_cat.fillna(name_missing)
+      
+    #Calculando a a cadinalidade das variaveis que sera usado como filtro
+    Cardinality = []
+    Feature = []
+    for f in dataframe_cat.columns:
+        Feature.append(f)
+        Cardinality.append(dataframe_cat[f].value_counts().shape[0])
+    
+    #Salvando o resumo pre-filtro
+    Resumo = pd.DataFrame()
+    Resumo['Variaveis'] = Feature
+    Resumo['Cardinality'] = Cardinality
+    
+    #Filtro com base na cadinalidade
+    drop = list(Resumo[(Resumo.Cardinality > cardinality)]['Variaveis'])
+    dataframe_cat = dataframe_cat.drop(drop,axis=1)
+    list_var_cat = dataframe_cat.columns
+
+    #Funçõe de geração de dummies
+    dataframe_cat = pd.get_dummies(dataframe_cat, 
+                                   columns = list_var_cat,
+                                   drop_first = drop_first, 
+                                   prefix = list_var_cat,
+                                   prefix_sep = '_')
+        
+    #Trazendo o index da base de entrada
+    final = dataframe_cat.set_index(dataframe.index)
+  
+    return dataframe_cat
+    
+  
---
+++ get_woe_categoric.py
@@ -0,0 +1,62 @@
+def get_woe_categoric(dataframe, target = 'target', name_missing = "missing"):
+
+    bons = dataframe[target] == 0
+    maus = dataframe[target] == 1
+
+    #Pegando somente as categoricas
+    dataframe_cat = dataframe[dataframe.select_dtypes('object').columns]
+    dataframe_cat = dataframe_cat.fillna(name_missing)
+    transform_vars_list = dataframe_cat.columns
+
+    df_final = dataframe_cat.drop(dataframe_cat.columns,axis = 1)
+
+    WOE = pd.DataFrame(columns = {'Value_Max','Bons', 'Maus', 'Variavel','ODDS','PD','WoE','IV Parcial'})
+
+    for f in dataframe_cat.columns:
+
+    ##########################################
+    ######### CALCULANDO O WOE ###############
+    ##########################################
+
+        # Contagem de bons e maus por categoria
+        tab_bons = pd.crosstab(dataframe_cat[f], columns='#', aggfunc='sum', dropna=False, values=bons)
+        tab_maus = pd.crosstab(dataframe_cat[f], columns='#', aggfunc='sum', dropna=False, values=maus)
+
+        # Montagem das variáveis
+        tabela_final = pd.DataFrame({'Bons':tab_bons['#']/tab_bons['#'].sum(),
+                                     'Maus':tab_maus['#']/tab_maus['#'].sum()})
+
+        #Truncamento   
+        tabela_final['Variavel'] = f
+        tabela_final['Bons'] = tabela_final['Bons'].replace(0, 0.0001)
+        tabela_final['Maus'] = tabela_final['Maus'].replace(0, 0.0001)
+        tabela_final['ODDS'] = (tab_bons['#']/tab_bons['#'].sum())/(tab_maus['#']/tab_maus['#'].sum())
+        tabela_final['PD'] = tab_maus['#'] / (tab_bons['#'] + tab_maus['#'])
+        tabela_final['WoE'] = np.log(tabela_final['Bons']/tabela_final['Maus'])
+        tabela_final['IV Parcial'] = (tabela_final['Bons'] - tabela_final['Maus'])*tabela_final['WoE'] 
+        tabela_final.reset_index(inplace = True) 
+        tabela_final.rename(columns = {f: 'Value_Max'}, inplace = True)
+
+        WOE = pd.concat([WOE,tabela_final],axis = 0)
+
+    ###########################################################
+    ######### SUBSTITUINDO O WOE NAS CATEGORIAS ###############
+    ###########################################################
+
+    for f in dataframe_cat.columns:
+        small_df = WOE[WOE['Variavel'] == f]
+        transform_dict = dict(zip(small_df.Value_Max,small_df.WoE))
+        replace_cmd = ''
+        replace_cmd1 = ''
+        for i in sorted(transform_dict.items()):
+            replace_cmd = replace_cmd + str(i[1]) + str(' if x <= ') + str(i[0]) + ' else '
+            replace_cmd1 = replace_cmd1 + str(i[1]) + str(' if x == "') + str(i[0]) + '" else '
+        replace_cmd = replace_cmd + '0'
+        replace_cmd1 = replace_cmd1 + '0'
+        if replace_cmd != '0':
+            try:
+                df_final['woe_' + f] = dataframe_cat[f].apply(lambda x: eval(replace_cmd))
+            except:
+                df_final['woe_' + f] = dataframe_cat[f].apply(lambda x: eval(replace_cmd1))
+
+    return df_final,WOE
